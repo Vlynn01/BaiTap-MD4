34aa0604f7b96ec01b850446570dee34c3b28cd1
Vlynn Noo <89231237+Vlynn01@users.noreply.github.com>
2022-12-05 16:21:51 +0700

    Add files via upload

---
+++ Spring_Security/src/main/java/com/codegym/security/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.codegym.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//Đăng ký springSecurityFilterChain
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
